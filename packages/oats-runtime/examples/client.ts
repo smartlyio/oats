ec80c8d29a13fe72949cb113b19339b3d96a98c1
sakari jokinen <sakariij@gmail.com>
2019-05-09 16:24:54 +0300

    import everything through index

--- packages/oats-runtime/examples/client.ts
+++ packages/oats-runtime/examples/client.ts
@@ -1,18 +1,18 @@
 // yarn ts-node examples/client.ts
 import * as api from "../tmp/client.generated";
-import { json, axiosAdapter } from "../src/client";
+import { client } from "../index";
 import * as app from "./server";
 
 // 'api.client' is the abstract implementation of the client which is then
 // mapped to axios requests using 'axiosAdapter'
-const client = api.client(axiosAdapter);
+const apiClient = api.client(client.axiosAdapter);
 async function runClient() {
   try {
-    const posted = await client.item.post({
-      body: json({ id: "id", name: "name" })
+    const posted = await apiClient.item.post({
+      body: client.json({ id: "id", name: "name" })
     });
     if (posted.status === 201) {
-      const got = await client.item(posted.value.value.id).get({});
+      const got = await apiClient.item(posted.value.value.id).get({});
       if (got.status === 200 && got.value.value.id === "id") {
         process.exit(0);
       }
