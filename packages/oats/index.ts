4b4e27dfb8908bb36bffb752b7d7171ef856f9e5
sakari jokinen <sakariij@gmail.com>
2019-05-09 16:24:54 +0300

    import everything through index

---
+++ packages/oats/index.ts
@@ -0,0 +1,6 @@
+import * as runtime from "./src/runtime";
+import * as client from "./src/client";
+import * as server from "./src/server";
+import * as driver from "./src/driver";
+
+export { client, runtime, server, driver };
