31efb002f3da17e7d95fb9507e565afa996e16f3
sakari jokinen <sakariij@gmail.com>
2019-05-09 16:06:31 +0300

    init

---
+++ packages/oats-runtime/README.template.md
@@ -0,0 +1,22 @@
+# oats
+
+Generator for typescript clients and servers from openapi3 specs
+
+## Server usage
+
+```js
+>>examples/server.ts
+```
+
+## Client usage
+
+```js
+>>examples/client.ts
+```
+
+## Generating clients and servers
+
+```
+>>examples/driver.ts
+```
+
---
+++ packages/oats-runtime/test/server.spec.ts
@@ -0,0 +1,34 @@
+import * as server from '../src/server';
+import * as oar from '../src/runtime';
+
+describe('safe', () => {
+  it('validates request and response', async () => {
+    const endpoint = server.safe<any, any, any, any>(
+      oar.makeVoid(),
+      oar.makeObject({ param: oar.makeNumber() }) as oar.Maker<any, any>,
+      oar.makeVoid(),
+      oar.makeObject({
+        status: oar.makeNumber(200),
+        value: oar.makeObject({
+          contentType: oar.makeString(),
+          value: oar.makeString()
+        })
+      }),
+      async () => {
+        return oar.json(200, 'response');
+      }
+    );
+
+    const response = await endpoint({
+      method: 'get',
+      servers: ['https://example.com'],
+      path: 'some/path',
+      op: 'someOp',
+      params: null as any,
+      query: { param: 1 } as any,
+      body: null as any
+    });
+    expect(response.status).toEqual(200);
+    expect(response.value.value).toEqual('response');
+  });
+});
