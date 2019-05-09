fec93a926203b5d7588594175de09cc68cbb4cb1
sakari jokinen <sakariij@gmail.com>
2019-05-09 16:06:31 +0300

    init

---
+++ packages/oats-fast-check/src/driver.ts
@@ -0,0 +1,71 @@
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+import * as types from './generate-types';
+import * as server from './generate-server';
+import * as path from 'path';
+import * as oas from 'openapi3-ts';
+
+function modulePath(importer: string, module: string) {
+  let p = path.relative(path.dirname(importer), path.dirname(module));
+  if (p[0] !== '.') {
+    p = './' + p;
+  }
+  if (p[p.length - 1] !== '/') {
+    p = p + '/';
+  }
+  p = p + path.basename(module, '.ts');
+  return p;
+}
+
+export interface Driver {
+  openapiFilePath: string;
+  generatedValueClassFile: string;
+  header: string;
+  generatedServerFile?: string;
+  generatedClientFile?: string;
+  runtimeFilePath: string;
+  emitStatusCode?: (statusCode: number) => boolean;
+}
+
+function emitAllStatusCodes() {
+  return true;
+}
+export function generate(driver: Driver) {
+  const file = fs.readFileSync(driver.openapiFilePath, 'utf8');
+  const spec: oas.OpenAPIObject = yaml.load(file);
+  fs.writeFileSync(
+    driver.generatedValueClassFile,
+    driver.header +
+      '\n' +
+      types.run({
+        oas: spec,
+        runtimeModule: modulePath(driver.generatedValueClassFile, driver.runtimeFilePath),
+        emitStatusCode: driver.emitStatusCode || emitAllStatusCodes
+      })
+  );
+
+  if (driver.generatedClientFile) {
+    fs.writeFileSync(
+      driver.generatedClientFile,
+      server.run({
+        oas: spec,
+        runtimePath: modulePath(driver.generatedClientFile, driver.runtimeFilePath),
+        typePath: modulePath(driver.generatedClientFile, driver.generatedValueClassFile),
+        shapesAsResponses: false,
+        shapesAsRequests: true
+      })
+    );
+  }
+  if (driver.generatedServerFile) {
+    fs.writeFileSync(
+      driver.generatedServerFile,
+      server.run({
+        oas: spec,
+        runtimePath: modulePath(driver.generatedServerFile, driver.runtimeFilePath),
+        typePath: modulePath(driver.generatedServerFile, driver.generatedValueClassFile),
+        shapesAsRequests: false,
+        shapesAsResponses: true
+      })
+    );
+  }
+}
---
+++ packages/oats-fast-check/src/server.ts
@@ -0,0 +1,238 @@
+import * as Router from 'koa-router';
+import * as Koa from 'koa';
+import * as oar from './runtime';
+import * as assert from 'assert';
+import safeNavigation from '@smartlyio/safe-navigation';
+
+export interface Response<Status extends number, ContentType, Value> {
+  status: Status;
+  value: {
+    contentType: ContentType;
+    value: Value;
+  };
+}
+
+export interface RequestBody<A> {
+  contentType: string;
+  value: A;
+}
+
+export interface Params {
+  [param: string]: unknown;
+}
+export interface Query {
+  [key: string]: unknown;
+}
+
+export interface EndpointArg<
+  P extends Params | void,
+  Q extends Query | void,
+  Body extends RequestBody<any> | void
+> {
+  path: string;
+  method: Methods;
+  servers: string[];
+  op?: string;
+  params: P;
+  query: Q;
+  body: Body;
+}
+export type Endpoint<
+  P extends Params | void,
+  Q extends Query | void,
+  Body extends RequestBody<any> | void,
+  R extends Response<number, any, any>
+> = (ctx: EndpointArg<P, Q, Body>) => Promise<R>;
+
+export type SafeEndpoint = Endpoint<
+  Params | undefined,
+  Query | undefined,
+  RequestBody<any> | undefined,
+  Response<number, any, any>
+>;
+
+export interface MethodHandlers {
+  get?: SafeEndpoint;
+  post?: SafeEndpoint;
+  put?: SafeEndpoint;
+  delete?: SafeEndpoint;
+  patch?: SafeEndpoint;
+  options?: SafeEndpoint;
+}
+
+export interface Endpoints {
+  [opOrUrl: string]: MethodHandlers;
+}
+
+export class RequestValidationError extends Error {
+  constructor(public tag: string, public errors: oar.ValidationError[]) {
+    super('invalid request ' + tag + ' ' + errors.map(oar.validationErrorPrinter).join('\n'));
+  }
+}
+
+export class ResponseValidationError extends Error {
+  constructor(
+    public tag: string,
+    public originalResponse: any,
+    public errors: oar.ValidationError[]
+  ) {
+    super('invalid response ' + tag + ' ' + errors.map(oar.validationErrorPrinter).join('\n'));
+  }
+}
+
+function throwRequestValidationError(tag: string, e: oar.Make<any>) {
+  throw new RequestValidationError(tag, e.errors);
+  return null as any;
+}
+
+function throwResponseValidationError(tag: string, originalValue: any, e: oar.Make<any>) {
+  throw new ResponseValidationError(tag, originalValue, e.errors);
+  return null as any;
+}
+
+function voidify(value: {} | undefined) {
+  if (value == null || Object.keys(value).length > 0) {
+    return value;
+  }
+  return null;
+}
+
+export function safe<
+  P extends Params,
+  Q extends Query,
+  Body extends RequestBody<any>,
+  R extends Response<any, any, any>
+>(
+  params: oar.Maker<any, P>,
+  query: oar.Maker<any, Q>,
+  body: oar.Maker<any, Body>,
+  response: oar.Maker<any, R>,
+  endpoint: Endpoint<P, Q, Body, R>
+): Endpoint<Params, Query, RequestBody<any>, Response<number, any, any>> {
+  return async ctx => {
+    const result = await endpoint({
+      path: ctx.path,
+      method: ctx.method,
+      servers: ctx.servers,
+      op: ctx.op,
+      params: params(voidify(ctx.params)).success(throwRequestValidationError.bind(this, 'params')),
+      query: query(voidify(ctx.query)).success(throwRequestValidationError.bind(this, 'query')),
+      body: body(voidify(ctx.body)).success(throwRequestValidationError.bind(this, 'body'))
+    });
+    return response(result).success(
+      throwResponseValidationError.bind(this, `body ${ctx.path}`, result.value.value)
+    );
+  };
+}
+
+export type Methods = keyof MethodHandlers;
+export const supportedMethods: Methods[] = ['get', 'post', 'put', 'patch', 'options', 'delete'];
+
+type AnyMaker = oar.Maker<any, any>;
+
+interface CheckingTree {
+  [path: string]: {
+    [method: string]: {
+      safeHandler: (e: Endpoint<any, any, any, any>) => SafeEndpoint;
+      op: string;
+    };
+  };
+}
+
+export interface Handler {
+  op?: string;
+  path: string;
+  servers: string[];
+  method: Methods;
+  query: AnyMaker;
+  body: AnyMaker;
+  params: AnyMaker;
+  response: AnyMaker;
+}
+
+function createTree(handlers: Handler[]): CheckingTree {
+  return handlers.reduce((memo: any, element) => {
+    if (!memo[element.path]) {
+      memo[element.path] = {};
+    }
+    memo[element.path][element.method] = {
+      safeHandler: (e: Endpoint<any, any, any, any>) =>
+        safe(element.params, element.query, element.body, element.response, e),
+      op: element.op
+    };
+    return memo;
+  }, {});
+}
+
+function koaAdapter(router: Router): ServerAdapter {
+  return (path: string, op: string, method: Methods, handler: SafeEndpoint) => {
+    const koaPath = path.replace(/{([^}]+)}/g, (m, param) => ":" + param);
+    (router as any)[method](koaPath, async (ctx: Koa.Context) => {
+      const files = (ctx as any).request.files;
+      let fileFields = {};
+      if (files) {
+        fileFields = Object.keys(files).reduce((memo: any, name) => {
+          memo[name] = new oar.File(files[name].path, files[name].size);
+          return memo;
+        }, {});
+      }
+      const contentType = ctx.request.type;
+      const value = { ...(ctx.request as any).body, ...fileFields };
+      const body = Object.keys(value).length > 0 ? { value, contentType } : undefined;
+      const result = await handler({
+        path,
+        method: assertMethod(ctx.method.toLowerCase()),
+        servers: [],
+        op,
+        params: ctx.params,
+        query: ctx.query,
+        body
+      });
+      ctx.status = result.status;
+      ctx.body = result.value.value;
+    });
+  };
+}
+
+export type HandlerFactory<Spec> = (adapter: ServerAdapter) => (spec: Spec) => void;
+
+export function koaBindRoutes<Spec>(handler: HandlerFactory<Spec>, spec: Spec): Router {
+  const router = new Router();
+  const adapter = koaAdapter(router);
+  handler(adapter)(spec);
+  return router;
+}
+
+function assertMethod(method: string): Methods {
+  assert(supportedMethods.indexOf(method as any) >= 0, 'unsupported method name ' + method);
+  return method as any;
+}
+
+type ServerAdapter = (path: string, op: string, method: Methods, handler: SafeEndpoint) => void;
+
+export function createHandlerFactory<Spec extends Endpoints>(
+  handlers: Handler[]
+): HandlerFactory<Spec> {
+  const tree = createTree(handlers);
+  return (adapter: ServerAdapter) => {
+    return (spec: Spec) => {
+      Object.keys(spec).forEach(path => {
+        const endpoint: MethodHandlers = spec[path];
+        Object.keys(endpoint).forEach(method => {
+          const methodHandler = (endpoint as any)[method];
+          const endpointWrapper = safeNavigation(tree)[path][method].$;
+          if (!endpointWrapper) {
+            assert.fail('unknown endpoint ' + method.toUpperCase() + ' ' + path);
+            return;
+          }
+          adapter(
+            path,
+            endpointWrapper.op,
+            assertMethod(method),
+            endpointWrapper.safeHandler(methodHandler)
+          );
+        });
+      });
+    };
+  };
+}
