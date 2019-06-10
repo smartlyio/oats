09f1c5690e8556aa8bd9be3aa112b9f259ab1fb8
Sakari Jokinen <sakariij@gmail.com>
2019-06-10 14:49:09 +0300

    Merge pull request #2 from smartlyio/add-prettier-and-lint
    Add prettier and lint

--- packages/oats-fast-check/src/driver.ts
+++ packages/oats-fast-check/src/driver.ts
@@ -1,22 +1,22 @@
-import * as fs from "fs";
-import * as yaml from "js-yaml";
-import * as types from "./generate-types";
-import * as server from "./generate-server";
-import * as path from "path";
-import * as oas from "openapi3-ts";
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+import * as types from './generate-types';
+import * as server from './generate-server';
+import * as path from 'path';
+import * as oas from 'openapi3-ts';
 
 function modulePath(importer: string, module: string | undefined) {
   if (!module) {
-    return "@smartlyio/oats";
+    return '@smartlyio/oats';
   }
   let p = path.relative(path.dirname(importer), path.dirname(module));
-  if (p[0] !== ".") {
-    p = "./" + p;
+  if (p[0] !== '.') {
+    p = './' + p;
   }
-  if (p[p.length - 1] !== "/") {
-    p = p + "/";
+  if (p[p.length - 1] !== '/') {
+    p = p + '/';
   }
-  p = p + path.basename(module, ".ts");
+  p = p + path.basename(module, '.ts');
   return p;
 }
 
@@ -34,18 +34,15 @@ function emitAllStatusCodes() {
   return true;
 }
 export function generate(driver: Driver) {
-  const file = fs.readFileSync(driver.openapiFilePath, "utf8");
+  const file = fs.readFileSync(driver.openapiFilePath, 'utf8');
   const spec: oas.OpenAPIObject = yaml.load(file);
   fs.writeFileSync(
     driver.generatedValueClassFile,
     driver.header +
-      "\n" +
+      '\n' +
       types.run({
         oas: spec,
-        runtimeModule: modulePath(
-          driver.generatedValueClassFile,
-          driver.runtimeFilePath
-        ),
+        runtimeModule: modulePath(driver.generatedValueClassFile, driver.runtimeFilePath),
         emitStatusCode: driver.emitStatusCode || emitAllStatusCodes
       })
   );
@@ -55,14 +52,8 @@ export function generate(driver: Driver) {
       driver.generatedClientFile,
       server.run({
         oas: spec,
-        runtimePath: modulePath(
-          driver.generatedClientFile,
-          driver.runtimeFilePath
-        ),
-        typePath: modulePath(
-          driver.generatedClientFile,
-          driver.generatedValueClassFile
-        ),
+        runtimePath: modulePath(driver.generatedClientFile, driver.runtimeFilePath),
+        typePath: modulePath(driver.generatedClientFile, driver.generatedValueClassFile),
         shapesAsResponses: false,
         shapesAsRequests: true
       })
@@ -73,14 +64,8 @@ export function generate(driver: Driver) {
       driver.generatedServerFile,
       server.run({
         oas: spec,
-        runtimePath: modulePath(
-          driver.generatedServerFile,
-          driver.runtimeFilePath
-        ),
-        typePath: modulePath(
-          driver.generatedServerFile,
-          driver.generatedValueClassFile
-        ),
+        runtimePath: modulePath(driver.generatedServerFile, driver.runtimeFilePath),
+        typePath: modulePath(driver.generatedServerFile, driver.generatedValueClassFile),
         shapesAsRequests: false,
         shapesAsResponses: true
       })
