94d05850c9e5c8a36190183ed2c0cdc218a37607
Sakari Jokinen <sakariij@gmail.com>
2019-08-27 08:48:07 +0300

    expose client sent headers (#5)
    * expose client sent headers
    
    We expose only the headers specified in the oa spec. And to do that need to do
    some hackery to lose all the other headers.
    
    We hack around no content which various clients deal with in various ways to forcing it to have text/plain content type

--- examples/client.ts
+++ examples/client.ts
@@ -2,25 +2,40 @@
 import * as api from "../tmp/client.generated";
 import { client } from "../index";
 import * as app from "./server";
+import * as assert from "assert";
 
 // 'api.client' is the abstract implementation of the client which is then
 // mapped to axios requests using 'axiosAdapter'
 const apiClient = api.client(client.axiosAdapter);
 async function runClient() {
-  try {
     const posted = await apiClient.item.post({
-      body: client.json({ id: "id", name: "name" })
+        headers: {
+            authorization: 'Bearer ^-^'
+        },
+        body: client.json({ id: "id", name: "name" })
     });
-    if (posted.status === 201) {
-      const got = await apiClient.item(posted.value.value.id).get({});
-      if (got.status === 200 && got.value.value.id === "id") {
-        process.exit(0);
-      }
+    if (posted.status !== 201) {
+        return assert.fail('wrong response');
     }
-  } catch (e) {}
-  process.exit(1);
+    const stored = await apiClient.item(posted.value.value.id).get();
+    if (stored.status !== 200) {
+        return assert.fail('wrong response');
+    }
+    assert(stored.value.value.id === 'id');
+
+    const  deleted = await apiClient.item(posted.value.value.id).delete();
+    assert(deleted.status === 204);
+    return;
 }
 
 // spin up the server
 const port = 12000;
-app.createApp().listen(port, runClient);
+app.createApp().listen(port, async () => {
+    try {
+        await runClient();
+        process.exit(0);
+    } catch(e) {
+        console.log(e);
+        process.exit(1);
+    }
+});
