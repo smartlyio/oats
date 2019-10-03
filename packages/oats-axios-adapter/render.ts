4d117fdad4c2870bac91f01bf110fd4382d46123
André Praça <andre@smartly.io>
2019-10-03 14:12:46 +0300

    Merge pull request #10 from smartlyio/ts-36
    Upgrade TS version to v3.6.3

--- packages/oats-axios-adapter/render.ts
+++ packages/oats-axios-adapter/render.ts
@@ -1,17 +1,19 @@
-import * as fs from "fs";
-import * as child from "child_process";
-import * as assert from "assert";
-import * as util from "util";
+// tslint:disable: no-floating-promises no-console
 
-const md: string = fs.readFileSync("README.template.md", "utf8");
+import * as fs from 'fs';
+import * as child from 'child_process';
+import * as assert from 'assert';
+import * as util from 'util';
+
+const md: string = fs.readFileSync('README.template.md', 'utf8');
 const examples: string[] = [];
 fs.writeFileSync(
-  "README.md",
+  'README.md',
   md.replace(/^>>(.*)/gm, (match, file) => {
     const m = file.trim();
-    const example: any = fs.readFileSync("./" + m, "utf8");
-    const runner = example.split("\n")[0].match(/\/\/(.*)/)[1];
-    assert(runner, "missing runner command");
+    const example: any = fs.readFileSync('./' + m, 'utf8');
+    const runner = example.split('\n')[0].match(/\/\/(.*)/)[1];
+    assert(runner, 'missing runner command');
     examples.push(runner.trim());
     return example;
   })
@@ -19,18 +21,18 @@ fs.writeFileSync(
 
 Promise.all(
   examples.map(async example => {
-    console.log("testing " + example);
+    console.log('testing ' + example);
     try {
       await util.promisify(child.exec)(example);
-      return example + ": ok";
+      return example + ': ok';
     } catch (e) {
-      return example + ": error " + e.message;
+      return example + ': error ' + e.message;
     }
   })
 ).then(results => {
   const errors = results.filter(result => /: error/.test(result));
   if (errors.length > 0) {
-    console.error("errors in rendering");
+    console.error('errors in rendering');
     errors.map(error => console.error(error));
     process.exit(1);
   }
