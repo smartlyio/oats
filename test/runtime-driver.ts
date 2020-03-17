42c604fa10df9e30f602d1ce18f483174fbe05ca
André Praça <andre@smartly.io>
2020-03-17 15:51:06 +0200

    Fix GHA

--- test/runtime-driver.ts
+++
@@ -1,9 +0,0 @@
-import { driver } from '../index';
-
-// generate type definitions for schemas from an external openapi spec
-driver.generate({
-  generatedValueClassFile: './runtime/tmp/fixture.types.generated.ts',
-  header: '/* tslint:disable variable-name only-arrow-functions*/',
-  openapiFilePath: './test/fixture.yaml',
-  runtimeFilePath: './runtime/src/runtime'
-});
