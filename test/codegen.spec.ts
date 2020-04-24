6c54b6cb06e3e927f7497fc2bdcd85c212acfe2a
sakari <sakariij@gmail.com>
2020-04-24 09:33:00 +0300

    note about type splitting

---
+++ test/codegen.spec.ts
@@ -0,0 +1,9 @@
+import * as types from '../tmp/common.types.generated';
+describe('codegen', () => {
+  it('splits nullable types', () => {
+    expect(types.typeNullable.maker(null).success()).toBeNull();
+    expect(types.typeNullable.maker({ field: 'abc' }).success()).toBeInstanceOf(
+      types.NonNullableNullable
+    );
+  });
+});
