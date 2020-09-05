e68f5a4aa3370ed48e359f1257ef021105af6b3b
sakari <sakariij@gmail.com>
2020-09-05 16:57:29 +0300

    handle race conditions when multiple files refer the same file

--- src/generate-types.ts
+++ src/generate-types.ts
@@ -36,6 +36,11 @@ export interface Options {
   };
 }
 
+export function info(message: string) {
+  // eslint-disable-next-line no-console
+  console.log(message);
+}
+
 export function deprecated(condition: any, message: string) {
   if (condition) {
     // eslint-disable-next-line no-console
@@ -78,9 +83,13 @@ const statusCodes = [
   504
 ];
 
+// list of files for which we have an action to generate it already
+// to prevent calling an action to generate it again and causing maybe some race conditions
+const generatedFiles: Set<string> = new Set();
+
 export function run(options: Options) {
   // eslint-disable-next-line no-console
-  console.log(`generating ${options.targetFile} from ${options.sourceFile}`);
+  info(`generating ${options.targetFile} from ${options.sourceFile}`);
   deprecated(
     options.externalOpenApiImports.length > 0,
     "'externalOpenApiImports' is deprecated. Consider using 'resolve' instead"
@@ -1415,11 +1424,15 @@ export function run(options: Options) {
   ) {
     if (!state.imports[importAs]) {
       if (importFile) {
-        // eslint-disable-next-line no-console
-        console.log('First import of ' + importAs + ' from file ' + importFile);
+        info('First import of ' + importAs + ' from file ' + importFile);
         state.imports[importAs] = importFile;
         if (action) {
-          state.actions.push(action);
+          if (generatedFiles.has(importFile)) {
+            info('already generating ' + importFile + ' skipping action');
+          } else {
+            generatedFiles.add(importFile);
+            state.actions.push(action);
+          }
         }
       }
     }
