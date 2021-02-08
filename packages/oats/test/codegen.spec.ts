7fc08475c1fe3fa16d58b7c8b2448efbd1205c0b
Sakari Jokinen <sakariij@gmail.com>
2021-02-08 15:38:28 +0200

    Merge pull request #180 from smartlyio/support-discriminator
    Add support for discriminator in oneOf

--- packages/oats/test/codegen.spec.ts
+++ packages/oats/test/codegen.spec.ts
@@ -1,14 +1,52 @@
-import * as types from '../tmp/client/common.types.generated';
+import * as common from '../tmp/client/common.types.generated';
+import * as types from '../tmp/client/types.generated';
+
 describe('codegen', () => {
   it('splits nullable types', () => {
-    expect(types.typeNullable.maker(null).success()).toBeNull();
-    expect(types.typeNullable.maker({ field: 'abc' }).success()).toBeInstanceOf(
-      types.NonNullableNullable
+    expect(common.typeNullable.maker(null).success()).toBeNull();
+    expect(common.typeNullable.maker({ field: 'abc' }).success()).toBeInstanceOf(
+      common.NonNullableNullable
     );
   });
 
   it('works correctly with boolean enums', () => {
-    expect(types.typeGuard.maker(true).success()).toBe(true);
-    expect(types.typeGuard.maker(false as any).success).toThrowError();
+    expect(common.typeGuard.maker(true).success()).toBe(true);
+    expect(common.typeGuard.maker(false as any).success).toThrowError();
+  });
+
+  it('oneOf with discriminator with implicit mappings works properly', () => {
+    expect(common.typeChoice.maker({ type: 'choice1', field: 'foo' }).success()).toEqual({
+      type: 'choice1',
+      field: 'foo'
+    });
+    expect(common.typeChoice.maker({ type: 'choice3', field3: 'foo' }).success()).toBeInstanceOf(
+      common.ChoiceItem3
+    );
+    expect(common.typeChoice.maker({ type: 'choice2', field: 'foo' } as any).errors).toEqual([
+      { error: 'unexpected property', path: ['field'] }
+    ]);
+    expect(
+      types.typeChoiceWithExternalRef.maker({ type: 'choice3', field3: 'foo' }).success()
+    ).toBeInstanceOf(common.ChoiceItem3);
+  });
+
+  it('oneOf with discriminator with explicit mappings works properly', () => {
+    expect(
+      types.typeChoiceWithExplicitMapping.maker({ type: 'choice4', field4: 'foo' }).success()
+    ).toEqual({
+      type: 'choice4',
+      field4: 'foo'
+    });
+    expect(
+      types.typeChoiceWithExplicitMapping.maker({ type: 'choice3', field3: 'foo' }).success()
+    ).toBeInstanceOf(common.ChoiceItem3);
+    expect(
+      types.typeChoiceWithExplicitMapping.maker({ type: 'choice2', field: 'foo' } as any).errors
+    ).toEqual([
+      {
+        error: 'unexpected value "choice2" in field "type" which is used as discriminator',
+        path: []
+      }
+    ]);
   });
 });
