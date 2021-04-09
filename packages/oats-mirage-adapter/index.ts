f8418e8b62d89226e56d54b9728b5514e5cc7eae
sakari <sakariij@gmail.com>
2021-04-09 09:45:09 +0300

    handle form-data requests

--- packages/oats-mirage-adapter/index.ts
+++ packages/oats-mirage-adapter/index.ts
@@ -5,6 +5,41 @@ import Schema from 'miragejs/orm/schema';
 import { Server } from 'miragejs/server';
 import { Registry as MirageRegistry, Registry } from 'miragejs/-types';
 
+function guessContentType(contentTypeHeader: string | undefined, value: any): string {
+  if (contentTypeHeader) {
+    return contentTypeHeader;
+  }
+  if (value instanceof FormData) {
+    return 'multipart/form-data';
+  }
+  return 'text/html';
+}
+
+async function guessValue(contentType: string, value: any) {
+  if (value instanceof FormData) {
+    return await handleFormData(value);
+  }
+  if (contentType.match(/application\/json/) && value) {
+    return JSON.parse(value);
+  }
+  return value;
+}
+
+async function handleFormData(value: FormData) {
+  const result: Record<string, string | ArrayBuffer> = {};
+  const keys: string[] = [];
+  value.forEach((_, key) => keys.push(key));
+  for (const key of keys) {
+    const keyValue = value.get(key);
+    if (keyValue instanceof Blob) {
+      result[key] = await keyValue.arrayBuffer();
+    } else if (typeof keyValue === 'string') {
+      result[key] = keyValue;
+    }
+  }
+  return result;
+}
+
 function adapter<Registry extends mirageTypes.AnyRegistry, RequestContext>(
   mirageServer: mirage.Server<Registry>,
   requestContextCreator: (schema: Schema<Registry>, request: mirage.Request) => RequestContext
@@ -18,15 +53,15 @@ function adapter<Registry extends mirageTypes.AnyRegistry, RequestContext>(
     const miragePath = path.replace(/{([^}]+)}/g, (m, param) => ':' + param);
     const mirageHandler: typeof mirageServer.get = (mirageServer as any)[method];
     mirageHandler(miragePath, async (schema, request) => {
-      const contentType = request.requestHeaders['content-type'] || 'text/html';
-      const value =
-        contentType.match(/application\/json/) && request.requestBody
-          ? JSON.parse(request.requestBody)
-          : request.requestBody;
+      const contentType = guessContentType(
+        request.requestHeaders['content-type'],
+        request.requestBody
+      );
+      const value = await guessValue(contentType, request.requestBody);
       const body = value != null ? { value, contentType } : undefined;
-      const result = await handler({
+      const ctx = {
         path,
-        method: runtime.server.assertMethod('get'),
+        method,
         servers: [],
         op,
         headers: request.requestHeaders,
@@ -34,16 +69,29 @@ function adapter<Registry extends mirageTypes.AnyRegistry, RequestContext>(
         query: request.queryParams,
         body,
         requestContext: requestContextCreator(schema, request)
-      });
+      };
       // eslint-disable-next-line no-console
-      console.log(result);
-      return new mirage.Response(
-        result.status,
-        { ...result.headers, 'content-type': result.value.contentType },
-        result.value.contentType.match(/application\/json/)
-          ? JSON.stringify(result.value.value)
-          : result.value.value
-      );
+      console.log('oats-mirage-adapter request', ctx);
+      try {
+        const result = await handler(ctx);
+        // eslint-disable-next-line no-console
+        console.log('oats-mirage-adapter response', { result });
+        return new mirage.Response(
+          result.status,
+          { ...result.headers, 'content-type': result.value.contentType },
+          result.value.contentType.match(/application\/json/)
+            ? JSON.stringify(result.value.value)
+            : result.value.value
+        );
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log('oats-mirage-adapter handler threw: ' + error.message, { error });
+        return new mirage.Response(
+          400,
+          { 'content-type': 'text/plain' },
+          'Error from handler: ' + error.message
+        );
+      }
     });
   };
 }
