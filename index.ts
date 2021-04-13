812a26348353ff64d139f86688b30bdbc9303016
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2021-04-13 09:17:16 +0200

    Export generate-types as well

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as driver from './src/driver';
 import * as util from './src/util';
+import * as types from './src/generate-types';
 
-export { driver, util };
+export { driver, util, types };
