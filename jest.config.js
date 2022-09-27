b76bc3b72d6d93a83673321fb736ad482ad3a812
Andre Praca <andre@smartly.io>
2022-09-27 15:17:02 +0300

    jest configs and fast-check unbump

--- jest.config.js
+++ jest.config.js
@@ -1,38 +1,29 @@
 module.exports = {
   moduleFileExtensions: ['js', 'ts', 'tsx'],
   moduleNameMapper: {
-      "@smartlyio/oats$": "<rootDir>/packages/oats/index.ts",
-      "@smartlyio/oats-runtime$": "<rootDir>/packages/oats-runtime/src/runtime.ts",
-      "@smartlyio/oats-axios-adapter$": "<rootDir>/packages/oats-axios-adapter/index.ts",
-      "@smartlyio/oats-koa-adapter$": "<rootDir>/packages/oats-koa-adapter/index.ts",
-      "@smartlyio/oats-fast-check$": "<rootDir>/packages/oats-fast-check/src/index.ts"
+    '@smartlyio/oats$': '<rootDir>/packages/oats/index.ts',
+    '@smartlyio/oats-runtime$': '<rootDir>/packages/oats-runtime/src/runtime.ts',
+    '@smartlyio/oats-axios-adapter$': '<rootDir>/packages/oats-axios-adapter/index.ts',
+    '@smartlyio/oats-koa-adapter$': '<rootDir>/packages/oats-koa-adapter/index.ts',
+    '@smartlyio/oats-fast-check$': '<rootDir>/packages/oats-fast-check/src/index.ts'
   },
-
-  testRegex: '.*\\.spec.(tsx?)$',
-
-  collectCoverageFrom: [
-    'packages/*/src/**/*.{ts,tsxx}',
-    '!packages/*/src/**/*.story.*'
-  ],
-
+  testRegex: '.*\\.spec.ts$',
+  collectCoverageFrom: ['packages/*/src/**/*.{ts,js}'],
   coverageThreshold: {
     global: {
       branches: 20,
-      statements: 20,
-    },
+      statements: 20
+    }
   },
-
   modulePathIgnorePatterns: ['<rootDir>/packages/*/dist'],
-
   transform: {
-    '^.+\\.(ts|js)x?$': 'ts-jest',
+    '^.+\\.(ts|js)x?$': 'ts-jest'
   },
-
   globals: {
     'ts-jest': {
       isolatedModules: true,
-      diagnostics: true,
-    },
+      diagnostics: true
+    }
   },
-  setupFilesAfterEnv: [],
+  setupFilesAfterEnv: []
 };
