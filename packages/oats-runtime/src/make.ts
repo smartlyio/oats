e7c410573b5965153327e41a83efb36f9a7d1cd2
Andres <ledancs@users.noreply.github.com>
2023-03-01 11:51:30 +0200

    fix: skip props with undefined value

--- packages/oats-runtime/src/make.ts
+++ packages/oats-runtime/src/make.ts
@@ -457,6 +457,10 @@ export function makeObject<
       result[index] = propResult.success();
     }
     for (const index of Object.keys(value)) {
+      // do not consider props that have undefined value
+      if (value[index] === undefined) {
+        continue;
+      }
       // do not consider props that got mapped
       if (opts?.convertFromNetwork && listedInputPropNames.has(index)) {
         continue;
