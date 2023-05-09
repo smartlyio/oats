3eed886b2a6c72fd0e8835011417de950bd8c2c0
Petri Miikki <petri.miikki@smartly.io>
2023-05-09 11:43:39 +0300

    Add possibility drop additional properties when client is handling response from server

--- test/request-parsing/client-body-send.spec.ts
+++ test/request-parsing/client-body-send.spec.ts
@@ -1,18 +1,19 @@
 // yarn ts-node examples/server.ts
 import { describe, it, expect, beforeAll } from '@jest/globals';
 import * as server from './tmp/server/generated';
+import * as client from './tmp/client/generated';
 import * as runtime from '@smartlyio/oats-runtime';
 import * as koaAdapter from '@smartlyio/oats-koa-adapter';
 import * as Koa from 'koa';
 import { koaBody } from 'koa-body';
+import * as axiosAdapter from '@smartlyio/oats-axios-adapter';
 import * as http from 'http';
 import { AddressInfo } from 'net';
-import { Axios } from 'axios';
 
-describe('server body', () => {
+describe('client body send', () => {
   const getRoutes = () =>
     koaAdapter.bind(
-      server.createRouter({ validationOptions: { body: { unknownField: 'drop' } } }),
+      server.createRouter({ validationOptions: { body: { unknownField: 'fail' } } }),
       {
         '/item': {
           post: async ctx => {
@@ -39,44 +40,41 @@ describe('server body', () => {
       const server = createApp().listen(0, () => resolve(server));
     });
     const { port } = httpServer.address() as AddressInfo;
-    const axios = new Axios({
-      baseURL: `http://localhost:${port}`
+    const clientSpec = client.createClient({
+      validationOptions: { body: { unknownField: 'drop' } }
+    })(spec => {
+      return axiosAdapter.create()({ ...spec, servers: [`http://localhost:${port}`] });
     });
     return {
       httpServer,
-      axios
+      clientSpec
     };
   };
 
   let httpServer: http.Server;
-  let axios: Axios;
+  let clientSpec: client.ClientSpec;
   beforeAll(async () => {
     const response = await createServer();
     httpServer = response.httpServer;
-    axios = response.axios;
+    clientSpec = response.clientSpec;
   });
 
   afterAll(() => {
     httpServer?.close();
   });
 
-  it('should drop all unknown values and process request when dropping unknown fields', async () => {
-    const actual = await axios.post(
-      '/item',
-      JSON.stringify({
-        thisIsUnkownProperty: 'value',
-        requiredField: 'xxx',
-        optionalField: 'yyy'
-      }),
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-    );
+  it('should drop all unknown values before sending request to server when dropping unknown fields', async () => {
+    const body = {
+      thisIsUnkownProperty: 'value',
+      requiredField: 'xxx',
+      optionalField: 'yyy'
+    } as any;
+    const actual = await clientSpec.item.post({
+      body: runtime.client.json(body)
+    });
 
     expect(actual.status).toBe(200);
-    expect(JSON.parse(actual.data)).toEqual({
+    expect(actual.value.value).toEqual({
       body: {
         requiredField: 'xxx',
         optionalField: 'yyy'
@@ -85,29 +83,25 @@ describe('server body', () => {
   });
 
   it('should drop unknown fields and match to closest schema', async () => {
-    const actual = await axios.post(
-      '/item',
-      JSON.stringify({
-        requiredField: 'xxx',
-        optionalField: 'yyy',
-        typeUnion: {
-          field_a: 'a',
-          field_b: 'b',
-          unknownField: true
-        }
-      }),
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
+    const body = {
+      requiredField: 'yyy',
+      optionalField: 'xxx',
+      typeUnion: {
+        field_a: 'a',
+        field_b: 'b',
+        unknownField: true
       }
-    );
+    } as any;
+
+    const actual = await clientSpec.item.post({
+      body: runtime.client.json(body)
+    });
 
     expect(actual.status).toBe(200);
-    expect(JSON.parse(actual.data)).toEqual({
+    expect(actual.value.value).toEqual({
       body: {
-        requiredField: 'xxx',
-        optionalField: 'yyy',
+        requiredField: 'yyy',
+        optionalField: 'xxx',
         typeUnion: {
           field_a: 'a',
           field_b: 'b'
@@ -116,24 +110,20 @@ describe('server body', () => {
     });
   });
 
-  it('should return error when request would match two different schemas with same amount of properties', async () => {
-    const actual = await axios.post(
-      '/item',
-      JSON.stringify({
-        requiredField: 'xxx',
-        optionalField: 'yyy',
-        typeUnion: {
-          field_a: 'a',
-          unknownField: true
-        }
-      }),
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
+  it('should throw error when object would match two different schemas with same amount of properties', async () => {
+    const body = {
+      requiredField: 'yyy',
+      optionalField: 'xxx',
+      typeUnion: {
+        field_a: 'a',
+        unknownField: true
       }
-    );
+    } as any;
 
-    expect(actual.status).toBe(500);
+    await expect(() =>
+      clientSpec.item.post({
+        body: runtime.client.json(body)
+      })
+    ).rejects.toThrow(/typeUnion: multiple options match/);
   });
 });
